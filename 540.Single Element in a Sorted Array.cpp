5cc70c8bb308b2100f04f2743a7507a818b32363
Akhand Pratap Shukla <145320759+A-P-Shukla@users.noreply.github.com>
2024-12-28 18:41:06 +0530

    Update 540.Single Element in a Sorted Array.cpp

--- 540.Single Element in a Sorted Array.cpp
+++ 540.Single Element in a Sorted Array.cpp
@@ -1,3 +1,4 @@
+//Code 1:
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
@@ -16,5 +17,41 @@ public:
             }
         }
         return nums[low];
+    }
+};
+
+//Code 2:
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        
+        if (n == 1) return nums[0];
+
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(mid == 0 && nums[0] != nums[1]) return nums[mid];
+            if(mid == n-1 && nums[n - 1] != nums[n - 2]) return nums[mid];
+
+            if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid +1]) return nums[mid];
+
+            if(mid % 2 == 0){
+                if (nums[mid - 1] == nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            else{
+                if (nums[mid - 1] == nums[mid]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
     }
 };
