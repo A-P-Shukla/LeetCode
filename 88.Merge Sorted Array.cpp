c909091787831a30663b89db6296eaedaa638cd8
Akhand Pratap Shukla <145320759+A-P-Shukla@users.noreply.github.com>
2025-01-02 00:16:29 +0530

    Update 88.Merge Sorted Array.cpp

--- 88.Merge Sorted Array.cpp
+++ 88.Merge Sorted Array.cpp
@@ -1,3 +1,4 @@
+//Brute Force:
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -5,5 +6,30 @@ public:
             nums1[m + i] = nums2[i];
         }
         sort(nums1.begin(), nums1.begin() + m + n);
+    }
+};
+
+//Two Pointers (Start from the end)
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
+        }
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
+        }
     }
 };
