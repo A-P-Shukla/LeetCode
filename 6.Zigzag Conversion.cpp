6fa56c66fff68e7f989b96bbf81dc67622b09096
Akhand Pratap Shukla <145320759+A-P-Shukla@users.noreply.github.com>
2025-01-07 20:14:31 +0530

    Update 6.Zigzag Conversion.cpp

--- 6.Zigzag Conversion.cpp
+++ 6.Zigzag Conversion.cpp
@@ -20,3 +20,22 @@ public:
         return result;
     }
 };
+
+//Code 2:
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1 || numRows >= s.size()) return s;
+        string result;
+        int cycleLen = 2 * numRows - 2;
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < s.size(); j += cycleLen) {
+                result += s[j + i];
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < s.size()) {
+                    result += s[j + cycleLen - i];
+                }
+            }
+        }
+        return result;
+    }
+};
