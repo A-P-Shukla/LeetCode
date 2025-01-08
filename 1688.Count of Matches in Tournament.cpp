e7d1a30fed47c730b8dd4873ad8bc515ab55ace4
Akhand Pratap Shukla <145320759+A-P-Shukla@users.noreply.github.com>
2025-01-08 16:40:23 +0530

    Update 1688.Count of Matches in Tournament.cpp

--- 1688.Count of Matches in Tournament.cpp
+++ 1688.Count of Matches in Tournament.cpp
@@ -24,3 +24,23 @@ public:
         return match;
     }
 };
+//Same Code But also Handles Edge Case:
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int match = 0;
+        if(n == 1) return 0;//Edge case
+        while(n != 1){
+            if(n % 2 == 0){
+                match += n / 2;
+                n = n/2;
+            }
+            else{
+                match += (n - 1)/2;
+                n = (n-1)/2;
+                n += 1;
+            }
+        }
+        return match;
+    }
+};
