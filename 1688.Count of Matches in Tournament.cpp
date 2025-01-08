23e3546c2c6afb9e276c8da69bebd77ae69a78e6
Akhand Pratap Shukla <145320759+A-P-Shukla@users.noreply.github.com>
2025-01-08 16:39:11 +0530

    Update 1688.Count of Matches in Tournament.cpp

--- 1688.Count of Matches in Tournament.cpp
+++ 1688.Count of Matches in Tournament.cpp
@@ -5,3 +5,22 @@ public:
         return n-1;
     }
 };
+//Coded Seriously
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int match = 0;
+        while(n>1){
+            if(n % 2 == 0){
+                match += n / 2;
+                n = n/2;
+            }
+            else{
+                match += (n - 1)/2;
+                n = (n-1)/2;
+                n += 1;
+            }
+        }
+        return match;
+    }
+};
