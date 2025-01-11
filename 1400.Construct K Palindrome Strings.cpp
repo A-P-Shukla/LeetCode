a88725eeb6c6f0065c58b0999507640dc1878664
Akhand Pratap Shukla <145320759+A-P-Shukla@users.noreply.github.com>
2025-01-11 23:58:13 +0530

    Update 1400.Construct K Palindrome Strings.cpp

--- 1400.Construct K Palindrome Strings.cpp
+++ 1400.Construct K Palindrome Strings.cpp
@@ -16,3 +16,22 @@ public:
         return charCount.size() <= k;
     }
 };
+
+//Optimized:
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        if (s.length() < k) return false;
+        vector<int> charCount(26, 0);
+        for (char c : s) {
+            charCount[c - 'a']++;
+        }
+        int oddCount = 0;
+        for (int count : charCount) {
+            if (count % 2 != 0) {
+                oddCount++;
+            }
+        }
+        return oddCount <= k;
+    }
+};
