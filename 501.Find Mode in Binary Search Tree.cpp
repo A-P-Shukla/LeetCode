46dfb967d44aedca0d2fdda67d909fb83527c79f
Akhand Pratap Shukla <145320759+A-P-Shukla@users.noreply.github.com>
2025-01-19 17:47:39 +0530

    Update 501.Find Mode in Binary Search Tree.cpp

--- 501.Find Mode in Binary Search Tree.cpp
+++ 501.Find Mode in Binary Search Tree.cpp
@@ -39,3 +39,38 @@ class Solution {
     pred = root;
   }
 };
+
+//Code 2:
+class Solution {
+ public:
+  TreeNode* previous;
+    int maxFrequency, currentCount;
+    vector<int> modes;
+    vector<int> findMode(TreeNode* root) {
+        previous = nullptr;
+        maxFrequency = 0;
+        currentCount = 0;
+        modes.clear();
+        inOrderTraversal(root);
+        return modes;
+    }
+    void inOrderTraversal(TreeNode* node) {
+        if (!node) return;
+        inOrderTraversal(node->left);
+        if (previous != nullptr && previous->val == node->val) {
+            currentCount++;
+        } else {
+            currentCount = 1;
+        }
+      
+        if (currentCount > maxFrequency) {
+            modes.clear();
+            modes.push_back(node->val);
+            maxFrequency = currentCount;
+        } else if (currentCount == maxFrequency) {
+            modes.push_back(node->val);
+        }
+        previous = node;
+        inOrderTraversal(node->right);
+    }
+};
