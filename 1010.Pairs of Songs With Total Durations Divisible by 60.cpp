97924e631c7a853380a50f026ff7b419db6795e3
Akhand Pratap Shukla <145320759+A-P-Shukla@users.noreply.github.com>
2025-01-20 23:13:31 +0530

    Update 1010.Pairs of Songs With Total Durations Divisible by 60.cpp

--- 1010.Pairs of Songs With Total Durations Divisible by 60.cpp
+++ 1010.Pairs of Songs With Total Durations Divisible by 60.cpp
@@ -48,3 +48,21 @@ class Solution {
     return ans;
   }
 };
+
+//Code 4:
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& times) {
+        int remainderCount[60] = {};
+        for (int& time : times) {
+            ++remainderCount[time % 60];
+        }
+        int pairsCount = 0;
+        for (int i = 1; i < 30; ++i) {
+            pairsCount += remainderCount[i] * remainderCount[60 - i];
+        }
+        pairsCount += static_cast<long long>(remainderCount[0]) * (remainderCount[0] - 1) / 2;
+        pairsCount += static_cast<long long>(remainderCount[30]) * (remainderCount[30] - 1) / 2;
+        return pairsCount;
+    }
+};
