69d42ea448298d8fc3f517e049d20d849d876ad9
Akhand Pratap Shukla <145320759+A-P-Shukla@users.noreply.github.com>
2026-03-08 23:19:36 +0530

    feat: add solution for 1980 - Find Unique Binary String

---
+++ 1980-find-unique-binary-string/solution.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    /**
+     * Finds a binary string of length n that does not appear in nums.
+     * We use Cantor's Diagonal Argument: flip the i-th bit of the i-th string.
+     */
+    std::string findDifferentBinaryString(std::vector<std::string>& nums) {
+        std::string result = "";
+        int n = nums.size();
+        
+        for (int i = 0; i < n; ++i) {
+            // If the i-th char of the i-th string is '0', append '1' to result.
+            // If it is '1', append '0'.
+            if (nums[i][i] == '0') {
+                result += '1';
+            } else {
+                result += '0';
+            }
+        }
+        
+        return result;
+    }
+};
+
+---CODE_END---
