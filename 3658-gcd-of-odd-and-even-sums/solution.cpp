c3fb804f779471e7c1496e29859458568e23c2a0
Akhand Pratap Shukla <akhandshukla36@gmail.com>
2026-07-15 23:04:44 +0530

    feat: add CPP solution for 3658 - GCD of Odd and Even Sums

---
+++ 3658-gcd-of-odd-and-even-sums/solution.cpp
@@ -0,0 +1,14 @@
+#include <numeric>
+
+class Solution {
+public:
+    /**
+     * Calculates the GCD of the sum of the first n odd numbers (n^2) 
+     * and the sum of the first n even numbers (n^2 + n).
+     * 
+     * Using GCD(n^2, n^2 + n) = GCD(n^2, n) = n.
+     */
+    int gcdOfOddAndEvenSums(int n) {
+        return n;
+    }
+};
