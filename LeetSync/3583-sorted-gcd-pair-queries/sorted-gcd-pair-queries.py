1ac74e1bb20219151547ea5b8e8375152afed84b
Akhand Pratap Shukla <145320759+A-P-Shukla@users.noreply.github.com>
2026-07-17 23:22:58 +0530

    Time: 2145 ms (7.50%) | Memory: 40.5 MB (37.50%) - LeetSync

---
+++ LeetSync/3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.py
@@ -0,0 +1,30 @@
+class Solution:
+    def gcdValues(self, nums: list[int], queries: list[int]) -> list[int]:
+        max_val = max(nums)
+        
+        freq = [0] * (max_val + 1)
+        for x in nums:
+            d = 1
+            while d * d <= x:
+                if x % d == 0:
+                    freq[d] += 1
+                    if d * d != x:
+                        freq[x // d] += 1
+                d += 1
+        
+        count = [0] * (max_val + 1)
+        for i in range(max_val, 0, -1):
+            pairs_with_multiple = (freq[i] * (freq[i] - 1)) // 2
+            for j in range(2 * i, max_val + 1, i):
+                pairs_with_multiple -= count[j]
+            count[i] = pairs_with_multiple
+            
+        prefix_sum = [0] * (max_val + 1)
+        for i in range(1, max_val + 1):
+            prefix_sum[i] = prefix_sum[i - 1] + count[i]
+            
+        results = []
+        for q in queries:
+            results.append(bisect_right(prefix_sum, q))
+            
+        return results
