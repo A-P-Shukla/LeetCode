41c3fea51964f93faf159c7d195b2d6b4d9dcde7
Akhand Pratap Shukla <akhandshukla36@gmail.com>
2026-07-27 11:58:03 +0530

    feat: add Python solution for 1464 - Maximum Product of Two Elements in an Array

---
+++ 1464-maximum-product-of-two-elements-in-an-array/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProduct(self, nums: list[int]) -> int:
+        max1 = 0
+        max2 = 0
+        
+        # Single pass to keep track of the top two largest numbers
+        for num in nums:
+            if num > max1:
+                max2 = max1
+                max1 = num
+            elif num > max2:
+                max2 = num
+                
+        return (max1 - 1) * (max2 - 1)
