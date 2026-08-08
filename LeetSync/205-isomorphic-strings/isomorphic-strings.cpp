8ec3998d2129f61cc33f424a57267013106bb588
Akhand Pratap Shukla <145320759+A-P-Shukla@users.noreply.github.com>
2026-08-08 21:54:14 +0530

    Time: 1 ms (54.80%) | Memory: 9.5 MB (30.80%) - LeetSync

---
+++ LeetSync/205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+
+        unordered_map<char, char> sTot;
+        unordered_map<char, char> tTos;
+
+        for (int i = 0; i < s.length(); i++) {
+            char a = s[i];
+            char b = t[i];
+
+            if (sTot.count(a) && sTot[a] != b)
+                return false;
+
+            if (tTos.count(b) && tTos[b] != a)
+                return false;
+
+            sTot[a] = b;
+            tTos[b] = a;
+        }
+
+        return true;
+    }
+};
