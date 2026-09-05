090d189703c421b154df594b6134a6fe32a2f69a
A-P-Shukla <akhandshukla36@gmail.com>
2026-09-05 11:04:14 +0530

    feat(python): add Smallest Stable Index II solution

---
+++ 3904-smallest-stable-index-ii/solution.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def firstStableIndex(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        
+        # suffix_min[i] will store the minimum value in nums[i..n-1]
+        suffix_min = [0] * n
+        suffix_min[-1] = nums[-1]
+        
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(nums[i], suffix_min[i + 1])
+            
+        # Track the running maximum of nums[0..i]
+        prefix_max = nums[0]
+        for i in range(n):
+            prefix_max = max(prefix_max, nums[i])
+            
+            # Check if the instability score is <= k
+            if prefix_max - suffix_min[i] <= k:
+                return i
+                
+        return -1
