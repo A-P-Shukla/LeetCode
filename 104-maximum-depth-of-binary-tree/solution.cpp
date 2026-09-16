9d0517f47fb8c600e92fe921a21101f8b725f985
Akhand Pratap Shukla <145320759+A-P-Shukla@users.noreply.github.com>
2026-09-16 17:51:21 +0530

    feat(cpp, python): add Maximum Depth of Binary Tree solution

---
+++ 104-maximum-depth-of-binary-tree/solution.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    /**
+     * @brief Returns the maximum depth (number of nodes along the longest root-to-leaf path).
+     */
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
